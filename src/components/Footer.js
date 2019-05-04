8cd305e50da21a8db72c85d1a96a5085d964c358
Nick <nick.tchemi@gmail.com>
2019-05-03 21:39:04 -0700

    Updated footer github link.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,17 @@ import githubLogo from "../assets/GitHub-Mark-Light-32px.png";
 const Footer = () => (
   <>
     <p className="footer">
-      Made with the <b><a href="https://www.coindesk.com/api">CoinDesk API</a></b>.
+      Made with the{" "}
+      <b>
+        <a href="https://www.coindesk.com/api">CoinDesk API</a>
+      </b>
+      .
       <br />
       Nick Tchemitcheff
       <br />
       2019
       <br />
-      <a href="https://github.com/usaspiff/workout-test-3">
+      <a href="https://github.com/usaspiff/Bitcoin-Rate_React_App">
         <img
           style={{ marginTop: "10px" }}
           src={githubLogo}
